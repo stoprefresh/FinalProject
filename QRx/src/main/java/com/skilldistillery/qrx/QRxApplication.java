c4728d30ded052c419459093063f081872017527
Miguel <marsigliamiguel@protonmail.com>
2019-08-15 14:53:57 -0600

    Boot Rest Stubbed, JPAQRx linked with dependencies

---
+++ QRx/src/main/java/com/skilldistillery/qrx/QRxApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.qrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QRxApplication.class, args);
+	}
+
+}
---
+++ QRx/src/test/java/com/skilldistillery/qrx/QRxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.qrx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QRxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
