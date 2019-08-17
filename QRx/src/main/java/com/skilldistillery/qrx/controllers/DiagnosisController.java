3c911a338f3aff82e78f4fa7f07445d358f0bc34
cwk9852 <cecil.kitch@gmail.com>
2019-08-17 09:54:24 -0600

    Entity Testing

--- QRx/src/main/java/com/skilldistillery/qrx/controllers/DiagnosisController.java
+++ QRx/src/main/java/com/skilldistillery/qrx/controllers/DiagnosisController.java
@@ -1,10 +1,6 @@
 package com.skilldistillery.qrx.controllers;
 
-import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- QRx/src/main/java/com/skilldistillery/qrx/repositories/EmergencyContactRepository.java
+++ QRx/src/main/java/com/skilldistillery/qrx/repositories/EmergencyContactRepository.java
@@ -1,12 +1,9 @@
 package com.skilldistillery.qrx.repositories;
 
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.qrx.entities.EmergencyContact;
 
 public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Integer>{
-	Set <EmergencyContact> findById_EmergencyContactId(int id);
 
 }
