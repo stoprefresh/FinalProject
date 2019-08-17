d2b49b8bd7532957698549bbcabc10c99fd5b1d0
Newlifedirection <craig.dreiling@gmail.com>
2019-08-17 14:28:49 -0600

    diagnosis cont, svc, svcimpl, repo

--- QRx/src/main/java/com/skilldistillery/qrx/repositories/DiagnosisRepository.java
+++ QRx/src/main/java/com/skilldistillery/qrx/repositories/DiagnosisRepository.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.qrx.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.qrx.entities.Diagnosis;
 
 public interface DiagnosisRepository extends JpaRepository<Diagnosis, Integer>{
-
+	List <Diagnosis> findAllByPatient_Id(Integer pid);
+	Diagnosis findAllByIdAndPatient_Id(Integer did, Integer pid);
+	
+	
+	
 }
