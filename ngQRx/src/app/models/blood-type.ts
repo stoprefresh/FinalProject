3bf4119a868d7aa106f2bf394194eda019911ca8
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-08-20 15:27:31 -0600

    added blood type and dx

--- ngQRx/src/app/models/blood-type.ts
+++ ngQRx/src/app/models/blood-type.ts
@@ -1,3 +1,11 @@
 export class BloodType {
-  // TODO
+  id: number;
+  abo: string;
+  rh: boolean;
+
+  constructor(id?: number, abo?: string, rh?: boolean) {
+    this.id = id;
+    this.abo = abo;
+    this.rh = rh;
+  }
 }
