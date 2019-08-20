4eb3c76c6b2be1c30d44cc7c6c75619b351d5b28
cwk9852 <cecil.kitch@gmail.com>
2019-08-20 15:19:21 -0600

    provider model complete

--- ngQRx/src/app/models/provider.ts
+++ ngQRx/src/app/models/provider.ts
@@ -1,2 +1,24 @@
+import { User } from './user';
+import { Patient } from './patient';
+
 export class Provider {
+  id: number;
+  user: User;
+  organization: string;
+  subunit: string;
+  patientsApproved: Patient[];
+
+  constructor(
+    id?: number,
+    user?: User,
+    organization?: string,
+    subunit?: string,
+    patientsApproved?: Patient[]
+  ) {
+    this.id = id;
+    this.user = user;
+    this.organization = organization;
+    this.subunit = subunit;
+    this.patientsApproved = patientsApproved;
+  }
 }
