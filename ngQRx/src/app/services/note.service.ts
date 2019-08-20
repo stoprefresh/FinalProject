fb076633b9482b88bfff17ad6697f9d3a644b31a
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-08-20 16:50:02 -0600

    note service

--- ngQRx/src/app/services/note.service.ts
+++ ngQRx/src/app/services/note.service.ts
@@ -1,9 +1,121 @@
+import { AuthoService } from './autho.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import { DatePipe } from '@angular/common';
+
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { Medication } from '../models/medication';
+import { Note } from '../models/note';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+private url = environment.baseUrl + 'api/patients/notes';
+editNote = null;
 
-  constructor() { }
+  constructor(
+    private datePipe: DatePipe,
+    private http: HttpClient,
+    private auth: AuthoService,
+    private router: Router
+  ) {}
+
+  // shows one note
+  show(id: any): Observable<Note> {
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+    if (this.auth.checkLogin()) {
+      return this.http.get<Note>(`${this.url}/${id}`, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('NoteService.show(): error retrieving');
+        })
+      );
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  // shows all notes for pt
+  index() {
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+    if (this.auth.checkLogin()) {
+      return this.http.get<Note[]>(this.url + '?sorted=true', httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('NoteService.index(): error retrieving list');
+        })
+      );
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  destroy(id: string | number) {
+    console.log(id);
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+    if (this.auth.checkLogin()) {
+      return this.http.delete<Note>(`${this.url}/${id}`, httpOptions);
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  create(note: Note) {
+    const credentials = this.auth.getCredentials();
+    if (this.auth.checkLogin()) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${credentials}`,
+          'X-Requested-With': 'XMLHttpRequest'
+        })
+      };
+      return this.http.post<Note>(this.url, note, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('NoteService.create(): error creating note');
+        })
+      );
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  update(note: Note) {
+    const credentials = this.auth.getCredentials();
+    if (this.auth.checkLogin()) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${credentials}`,
+          'X-Requested-With': 'XMLHttpRequest'
+        })
+      };
+      return this.http.put<Note>(this.url + '/' + note.id, note, httpOptions);
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
 }
