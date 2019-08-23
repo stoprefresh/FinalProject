232fafd852d5167515f9652ef05bdadde8745589
Miguel <marsigliamiguel@protonmail.com>
2019-08-23 12:43:44 -0600

    qr code gen complete

---
+++ ngQRx/src/app/services/qr-generator.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QrGeneratorService {
+
+
+
+
+  constructor() { }
+
+
+
+
+
+
+
+}
